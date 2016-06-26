1e036cf78c3b1c67d9f8463b7785168bcf27fab7
danblitzhou <danzhou1810@gmail.com>
2016-06-26 10:32:51 -0400

    Changed project name; redirect to login once logged out

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -3,14 +3,14 @@ angular.module('MyApp')
     $scope.isActive = function (viewLocation) {
       return viewLocation === $location.path();
     };
-    
+
     $scope.isAuthenticated = function() {
       return $auth.isAuthenticated();
     };
-    
+
     $scope.logout = function() {
       $auth.logout();
       delete $window.localStorage.user;
-      $location.path('/');
+      $location.path('/login');
     };
   });
